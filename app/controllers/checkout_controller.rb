76f4caed9340a72ee5d565b161f9a3342380921b
Niyanta Prasad <niyantaprasad@Niyantas-MacBook-Pro.local>
2023-08-30 13:11:57 +0530

    first commit

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,26 @@
+class CheckoutController < ApplicationController
+  
+  def create
+    product = Product.find(params[:id])
+    session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          unit_amount: product.price,
+          product_data: {
+            name: product.name,
+          },
+        },
+        quantity: 1
+      }],
+      mode: 'payment',
+      success_url: root_url,
+      cancel_url: root_url,
+    })
+    puts session.id
+    respond_to do |format|
+      format.js
+    end
+  end
+end
---
+++ db/migrate/20230830061730_add_sales_count_to_product.rb
@@ -0,0 +1,5 @@
+class AddSalesCountToProduct < ActiveRecord::Migration[6.1]
+  def change
+    add_column :products, :sales_count, :integer, null: :false, default: 0
+  end
+end
